3ed739d845fe3fe1d9ee137f3d4892a0b98bba04
malexan <alexander@matrunich.com>
2012-10-02 13:27:53 +0400

    clean.r file created

---
+++ clean.r
@@ -0,0 +1,8 @@
+# Clean file:
+# Convert column names to dates
+# Clean regions names
+#  Remove digits
+#  Remove duplicated blanks
+#  Convert "г." to "город"
+# Set administrative level for every region (Okrug / Oblast / Avto.okrug)
+
