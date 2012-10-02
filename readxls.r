d0533374f121426eec3e64d2efca2805bf094e5e
malexan <alexander@matrunich.com>
2012-10-02 12:10:34 +0400

    Test commit sas message

---
+++ readxls.r
@@ -0,0 +1,5 @@
+library(XLConnect)
+
+rawdata <- readWorksheetFromFile('./info-stat-07-2012/01 промышленность/005-013 индекс промышленного производства.xls', "к пред. месяцу", 4, 1, 96, 0, header = F)
+
+# 
